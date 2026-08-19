aa773c4887d635e55d90fce24cca8236ad66a425
asmabanu27-s <asmabanu279@outlook.com>
2026-08-20 00:45:25 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,12 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://gleeful-strudel-64d602.netlify.app"
+    ]
+}));
 app.use(express.json());
 
 app.use(
