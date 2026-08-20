81a609bf3b080bdf5da3d6ede4ee339f53e5c619
BHARATH PANAMGIPALLI <asmabanu279@outlook.com>
2026-08-20 08:06:24 +0530

    Fix Netlify function dependencies

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://gleeful-strudel-64d602.netlify.app"
+        "https://magenta-bienenstitch-b38612.netlify.app"
     ]
 }));
 app.use(express.json());
