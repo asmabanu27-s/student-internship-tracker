7c82f738ea48c687408cdcf1724c4b478a810d43
asmabanu27-s <asmabanu279@outlook.com>
2026-08-20 23:19:13 +0530

    routes add

--- backend/src/routes/applications.js
+++ backend/src/routes/applications.js
@@ -47,7 +47,7 @@ router.get("/", async (req, res) => {
     }
 });
 
-router.get("/id:", async (req, res) => {
+router.get("/:id", async (req, res) => {
        try {
         const { id } = req.params;
 
